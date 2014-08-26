570797c74229c714c66caf01026c95c253077696
Waseem Sadiq <waseem@fileboard.com>
2014-08-26 18:39:10 +0200

    Initial commit

---
+++ bin/import.sh
@@ -0,0 +1,4 @@
+# Copy Foundation source files from master.
+# Assumes foundation repo is cloned in the same parent folder as this repo.
+cp -r ../foundation/js/foundation/ vendor/assets/javascripts/foundation
+cp -r ../foundation/scss/* vendor/assets/stylesheets
